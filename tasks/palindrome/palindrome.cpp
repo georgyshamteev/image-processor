01af9842302b09f9ac7000a3b6bce7a415c4842c
Georgy Shamteev <georgy.shamteev@gmail.com>
2023-01-19 14:48:35 +0300

    palindrome 1

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,5 +1,18 @@
 #include "palindrome.h"
 
 bool IsPalindrome(const std::string& str) {
-    return false;
+    uint64_t r = str.size() - 1;
+    for (uint64_t l = 0; l < str.size(); ++l) {
+        if (str[l] == ' ') {
+            continue;
+        }
+        while (str[r] == ' ' and r > 0) {
+            --r;
+        }
+        if (str[l] != str[r]) {
+            return false;
+        }
+        --r;
+    }
+    return true;
 }
