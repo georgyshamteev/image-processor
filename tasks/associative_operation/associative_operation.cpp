432a39f08185c87ba18ebf6b2d03b36a4b3a3f83
Georgy Shamteev <georgy.shamteev@gmail.com>
2023-01-23 18:49:30 +0300

    final

--- tasks/associative_operation/associative_operation.cpp
+++ tasks/associative_operation/associative_operation.cpp
@@ -1,20 +1,14 @@
 #include "associative_operation.h"
 
 bool IsAssociative(const std::vector<std::vector<size_t>>& table) {
-    int flag = 1;
-    for (int i = 0; i < table.size(); ++i) {
-        for (int j = 0; j < table.size(); ++j) {
-            for (int k = 0; k < table.size(); ++k) {
+    for (size_t i = 0; i < table.size(); ++i) {
+        for (size_t j = 0; j < table.size(); ++j) {
+            for (size_t k = 0; k < table.size(); ++k) {
                 if (table[table[i][j]][k] != table[i][table[j][k]]) {
-                    flag = 0;
-                    break;
+                    return false;
                 }
             }
         }
     }
-    if (flag == 0) {
-        return false;
-    } else {
-        return true;
-    }
+    return true;
 }
