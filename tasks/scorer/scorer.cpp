e973b56344b1a48d246fcf54b330bfb9a3ae35bd
Georgy Shamteev <georgy.shamteev@gmail.com>
2023-02-05 12:29:38 +0300

    pair

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -1,5 +1,4 @@
 #include "scorer.h"
-#include <iostream>
 #include <map>
 
 bool EventComp(const Event* lhv, const Event* rhv) {
@@ -13,7 +12,7 @@ void SortEvents(std::vector<const Event*>& events) {
 ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
     ScoreTable score_table;
 
-    std::map<const StudentName, std::map<const TaskName, std::array<bool, 2> >> mp; // [1, 0]
+    std::map<const StudentName, std::map<const TaskName, std::pair<bool, bool>>> mp;  // [1, 0]
 
     std::vector<const Event*> ptrs;
 
@@ -25,34 +24,20 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
     SortEvents(ptrs);
 
     for (const Event* event : ptrs) {
-        std::cout << event->student_name << std::endl;
-        std::cout << event->task_name << std::endl;
         if (event->event_type == EventType::CheckFailed) {
-            std::cout << "CheckFailed" << std::endl;
-            mp[event->student_name][event->task_name][0] = false;
+            mp[event->student_name][event->task_name].first = false;
         } else if (event->event_type == EventType::CheckSuccess) {
-            std::cout << "CheckSuccess" << std::endl;
-
-            mp[event->student_name][event->task_name][0] = true;
+            mp[event->student_name][event->task_name].first = true;
         } else if (event->event_type == EventType::MergeRequestOpen) {
-            std::cout << "OPen" << std::endl;
-
-            mp[event->student_name][event->task_name][1] = true;
+            mp[event->student_name][event->task_name].second = true;
         } else if (event->event_type == EventType::MergeRequestClosed) {
-            std::cout << "Closed" << std::endl;
-
-            mp[event->student_name][event->task_name][1] = false;
+            mp[event->student_name][event->task_name].second = false;
         }
     }
 
-    for (std::pair<const StudentName, std::map<const TaskName, std::array<bool, 2>>>& student : mp) {
-        std :: cout << student.first << std::endl;
-        for (std::pair<const TaskName, std::array<bool, 2>>& task : student.second) {
-            std::cout << task.first << std::endl;
-            for (const bool& fl : task.second) {
-                std::cout << fl << std::endl;
-            }
-            if (task.second[0] == 1 && task.second[1] == 0) {
+    for (std::pair<const StudentName, std::map<const TaskName, std::pair<bool, bool>>>& student : mp) {
+        for (std::pair<const TaskName, std::pair<bool, bool>>& task : student.second) {
+            if (task.second.first && !task.second.second) {
                 score_table[student.first].insert(task.first);
             }
         }
