80ab1bb62e97e8971326a403b69ce61a395d7958
Georgy Shamteev <georgy.shamteev@gmail.com>
2023-02-05 16:22:26 +0300

    3

--- tasks/unixpath/unixpath.cpp
+++ tasks/unixpath/unixpath.cpp
@@ -2,11 +2,56 @@
 #include "unixpath.h"
 
 std::string NormalizePath(std::string_view current_working_dir, std::string_view path) {
-    if (path[0] == '/') {
-        return static_cast<std::string>(path);
-    }
-    std::deque<std::string> abs_path;
+    std::string s;
     std::string curr;
+    std::deque<std::string> abs_path;
+    std::string result;
+
+    if (path[0] == '/') {
+        for (const char& ch : path) {
+            if (ch == '/') {
+                if (s == ".") {
+
+                } else if (s == "..") {
+                    if (!abs_path.empty()) {
+                        abs_path.pop_back();
+                    }
+                } else if (s.empty()) {
+
+                } else {
+                    abs_path.push_back(s);
+                }
+                s = "";
+            } else if (ch == '.') {
+                s.push_back(ch);
+            } else {
+                s.push_back(ch);
+            }
+        }
+
+        if (!s.empty()) {
+            if (s == ".") {
+
+            } else if (s == "..") {
+                if (!abs_path.empty()) {
+                    abs_path.pop_back();
+                }
+            } else {
+                abs_path.push_back(s);
+            }
+        }
+
+        if (!abs_path.empty()) {
+            for (std::string& str : abs_path) {
+                result.push_back('/');
+                result.append(str);
+            }
+        } else {
+            result.push_back('/');
+        }
+        return result;
+    }
+
     for (const char& ch : current_working_dir) {
         if (ch == '/') {
             if (!curr.empty()) {
@@ -17,10 +62,11 @@ std::string NormalizePath(std::string_view current_working_dir, std::string_view
             curr.push_back(ch);
         }
     }
+
     if (!curr.empty()) {
         abs_path.push_back(curr);
     }
-    std::string s;
+
     for (const char& ch : path) {
         if (ch == '/') {
             if (s == ".") {
@@ -41,6 +87,7 @@ std::string NormalizePath(std::string_view current_working_dir, std::string_view
             s.push_back(ch);
         }
     }
+
     if (!s.empty()) {
         if (s == ".") {
 
@@ -53,7 +100,6 @@ std::string NormalizePath(std::string_view current_working_dir, std::string_view
         }
     }
 
-    std::string result;
     if (!abs_path.empty()) {
         for (std::string& str : abs_path) {
             result.push_back('/');
