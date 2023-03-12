a287a9f0b92c1b8a40a92ce757668ac000f1102c
Georgy Shamteev <georgy.shamteev@gmail.com>
2023-03-12 14:42:25 +0300

    2

--- tasks/robot/knight_topology.h
+++ tasks/robot/knight_topology.h
@@ -7,7 +7,7 @@ public:
     explicit KnightTopology(std::vector<std::vector<bool>> v) : Topology(v), game_field_(v) {
         game_field_ = Field(v);
     }
-    std::vector<Point> GetNeighbours(const Point& point) const;
+    std::vector<Point> GetNeighbours(const Point& point) const override;
 
 private:
     Field game_field_;
